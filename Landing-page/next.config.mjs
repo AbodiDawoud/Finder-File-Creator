abb2321e0c34ed7ce043bb0b88623e369fddd88b
AbodiDawood <mo7.daawoud@icloud.com>
2026-04-18 04:23:02 +0200

    New landing page

---
+++ Landing-page/next.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/finder-file-creator',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.githubusercontent.com',
+      },
+    ],
+  },
+}
+
+export default nextConfig
